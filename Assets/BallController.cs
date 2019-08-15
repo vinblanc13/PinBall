dcb30ea77fc6ad64c7868b2cbeae581ed3f333b6
megmin <phoenix.raid.0906@gmail.com>
2019-08-15 14:07:30 +0900

    PinBallのコミット
    課題作成

---
+++ Assets/BallController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+    private GameObject pointText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.pointText = GameObject.Find("PointText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.pointText.GetComponent<PointUI>().AddPoint(1);
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.pointText.GetComponent<PointUI>().AddPoint(5);
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            this.pointText.GetComponent<PointUI>().AddPoint(2);
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            this.pointText.GetComponent<PointUI>().AddPoint(3);
+        }
+    }
+}
---
+++ Assets/PointUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointUI : MonoBehaviour {
+
+    //ゲームオーバを表示するテキスト
+    private GameObject pointText;
+    private int point = 0;
+    public void AddPoint(int point)
+    {
+        this.point += point;
+        Debug.Log(this.point + "加算");
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        //シーン中のPointTextオブジェクトを取得
+        this.pointText = GameObject.Find("PointText");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.pointText.GetComponent<Text>().text = "Score " + point + "点";
+	}
+}
